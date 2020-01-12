36a00697ec922729093ba397fb0eadcfb850c0ba
Monica <Monica@DESKTOP-N5CTOJG>
2020-01-12 07:47:50 +0530

    Updated

--- src/main/java/listeners/DemoA.java
+++ src/main/java/listeners/DemoA.java
@@ -5,7 +5,7 @@ import org.testng.Reporter;
 //import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-//@Listeners(listeners.DemoListeners.class)
+//@Listeners(listeners.DemoListeners.class)(packagename
 public class DemoA 
 {
 @Test
